e281b4f1280397c6506fa164c6b2cc3f5f90b8b1
code810 <nadirssh@code.edu.az>
2023-10-23 12:41:52 +0400

    task1 metod2

--- c#lesson1/Program.cs
+++ c#lesson1/Program.cs
@@ -1,20 +1,32 @@
-﻿#region task1
-using System.ComponentModel.Design;
+﻿#region task1 metod1
 
-int number=int.Parse(Console.ReadLine());
-while(number>0)
-if (number > 999 && number < 10000) 
-{
-    Console.Write("7");
-    Console.Write(number);
-    Console.WriteLine("8");
-        break;
-}
-else 
-{
-        Console.WriteLine("Yalniz 4 reqemli eded daxil ede bilersiniz");
-        number = int.Parse(Console.ReadLine());
- 
-}
+//int number=int.Parse(Console.ReadLine());
+//while(number>0)
+//if (number > 999 && number < 10000) 
+//{
+//    Console.Write("7");
+//    Console.Write(number);
+//    Console.WriteLine("8");
+//        break;
+//}
+//else 
+//{
+//        Console.WriteLine("Yalniz 4 reqemli eded daxil ede bilersiniz");
+//        number = int.Parse(Console.ReadLine());
 
+//}
+
+#endregion
+
+#region task1 metod2
+
+
+int number = int.Parse(Console.ReadLine());
+
+string result = number switch
+{
+        _ when number > 999 && number < 10000 => "7" + number + "8",
+        _ => "4 reqemli eded daxil edilmelidir"
+};
+Console.WriteLine(result);
 #endregion
