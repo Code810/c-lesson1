28882c8658aef5db44986b185bc4d0a6471bca8c
code810 <nadirssh@code.edu.az>
2023-10-23 12:10:26 +0400

    task1metod1

--- c#lesson1/Program.cs
+++ c#lesson1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region task1
+using System.ComponentModel.Design;
+
+int number=int.Parse(Console.ReadLine());
+while(number>0)
+if (number > 999 && number < 10000) 
+{
+    Console.Write("7");
+    Console.Write(number);
+    Console.WriteLine("8");
+        break;
+}
+else 
+{
+        Console.WriteLine("Yalniz 4 reqemli eded daxil ede bilersiniz");
+        number = int.Parse(Console.ReadLine());
+ 
+}
+
+#endregion
