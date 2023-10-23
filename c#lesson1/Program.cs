27ee6a60e8842b5052c7f831a1860f65df6c4284
code810 <nadirssh@code.edu.az>
2023-10-23 13:19:14 +0400

    task4 ready

--- c#lesson1/Program.cs
+++ c#lesson1/Program.cs
@@ -44,20 +44,35 @@
 #endregion
 
 #region task3
-int number = int.Parse(Console.ReadLine());
-int result = number switch
-{
-    _ when number>9999&&number<100000 => (number*18)/100,
-    _=>0
+//int number = int.Parse(Console.ReadLine());
+//int result = number switch
+//{
+//    _ when number>9999&&number<100000 => (number*18)/100,
+//    _=>0
+//};
+//if (result==0)
+//{
+//    Console.WriteLine("5 reqemli eded daxil edin");
+//}
+//else
+//{
+//    Console.WriteLine(result);
+//    result = (result * 3) / 100;
+//    Console.WriteLine(result);
+//}
+#endregion
+
+#region task4
+using System.Diagnostics.CodeAnalysis;
+
+int number=int.Parse(Console.ReadLine());
+string result = number switch
+{ 
+  _ when number>99&&number<1000 => number+"7",
+  _=>"3 reqemli eded daxil edin"
 };
-if (result==0)
-{
-    Console.WriteLine("5 reqemli eded daxil edin");
-}
-else
-{
-    Console.WriteLine(result);
-    result = (result * 3) / 100;
-    Console.WriteLine(result);
-}
+float count = int.Parse(result);
+count = (count * 7) / 100;
+Console.Write("cavab:");
+Console.WriteLine(count);
 #endregion
